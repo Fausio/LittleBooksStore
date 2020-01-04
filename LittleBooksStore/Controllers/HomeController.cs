c804565f23b7043359665562f49895a0c5f9c599
fausio luis <fausioluis@live.com>
2020-01-04 23:40:46 +0200

    Sanding the list do index view

--- LittleBooksStore/Controllers/HomeController.cs
+++ LittleBooksStore/Controllers/HomeController.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LittleBooksStore.Models;
 
 namespace LittleBooksStore.Controllers
 {
     public class HomeController : Controller
     {
+        private List<Book> _book;
 
         //the  home page
         public IActionResult Index()
         {
-            return View();
+              return View(_book);
         }
 
 
@@ -26,6 +28,35 @@ namespace LittleBooksStore.Controllers
         public IActionResult Contact()
         {
             return View();
+        }
+
+
+        public HomeController()
+        {
+            _book = new List<Book>();
+            _book.Add(
+                new Book()
+                {
+                    Id = 1,
+                    Title = "The Witcher 3",
+                    Description = "Geralt the Wolf of Rivia",
+                    Author = "Fausio Matsinhe",
+                    PublishDate = "July, 2018",
+                    Price = 39.01,
+                    image = ""
+                });
+
+            _book.Add(
+                 new Book()
+                 {
+                     Id = 2,
+                     Title = "The Hobbit",
+                     Description = "Lord of the rings",
+                     Author = "Carlos Uchia",
+                     PublishDate = "Jan, 2020",
+                     Price = 39.01,
+                     image = ""
+                 });
         }
     }
 }
