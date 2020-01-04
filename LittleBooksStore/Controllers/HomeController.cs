d635e3434cdcf425d199c23c0559e45a613eb9d8
fausio luis <fausioluis@live.com>
2020-01-04 22:52:17 +0200

    Add project files.

---
+++ LittleBooksStore/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleBooksStore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //the  home page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // the  About page
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //the  contact page
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
