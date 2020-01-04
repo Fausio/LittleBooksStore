af4677750c694ec7e301ca5a359ca86a65453878
fausio luis <fausioluis@live.com>
2020-01-05 01:54:22 +0200

    Adding  MockCarouselRepository

---
+++ LittleBooksStore/Services/MockCarouselRepository.cs
@@ -0,0 +1,79 @@
+﻿using LittleBooksStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LittleBooksStore.Services
+{
+    public class MockCarouselRepository : IRepository<Carousel>
+    {
+        List<Carousel> _caroucel;
+
+        public bool Add(Carousel item)
+        {
+            try
+            {
+                var Carousel = item;
+                Carousel.Id = _caroucel.Max(c => c.Id) + 1;
+                _caroucel.Add(Carousel);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(Carousel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Carousel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Carousel Get(int id)
+        {
+            return _caroucel.FirstOrDefault(c => c.Id == id);
+        }
+
+        public IEnumerable<Carousel> GetAll()
+        {
+            return _caroucel.ToList();
+        }
+
+
+
+        public MockCarouselRepository()
+        {
+            _caroucel.Add(new Carousel()
+            {
+                Id = 0,
+                Title = "Discount Book",
+                Description = "Discount books get them all",
+                ImageURL = ""
+            });
+
+            _caroucel.Add(new Carousel()
+            {
+                Id = 0,
+                Title = "New Book",
+                Description = "All brand new books",
+                ImageURL = ""
+            });
+
+            _caroucel.Add(new Carousel()
+            {
+                Id = 0,
+                Title = "Sunscription",
+                Description = "Discount on monthly Sunscription",
+                ImageURL = ""
+            });
+
+        }
+    }
+}
