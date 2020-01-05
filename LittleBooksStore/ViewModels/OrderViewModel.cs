22927ee59662c0b4d5411a1bdab62b74f6faa0a9
fausio luis <fausioluis@live.com>
2020-01-05 21:12:52 +0200

    adding OrderViewModel

---
+++ LittleBooksStore/ViewModels/OrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LittleBooksStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LittleBooksStore.ViewModels
+{
+    public class OrderViewModel
+    {
+        public Book BookToOrder { get; set; }
+        public Order Order { get; set; }
+    }
+}
