5e5630b9d3af457867082566e8d413739ece6982
fausio luis <fausioluis@live.com>
2020-01-05 02:00:48 +0200

    adding HomeIndexViewModel

---
+++ LittleBooksStore/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LittleBooksStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LittleBooksStore.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Carousel> Carousels { get; set; }
+    }
+}
